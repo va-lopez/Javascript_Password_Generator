1c43c16a4942514d1fbfb134905287d844ea7ffd
valcomp <valerie.aleida.lopez@gmail.com>
2022-04-24 22:11:01 -0500

    added pseudocode

--- assets/images/README.md
+++ assets/images/README.md
@@ -34,7 +34,7 @@ THEN the password is either displayed in an alert or written to the page
 
 The following image demonstrates the application functionality:
 
-![An app window with the label Password Generator, an input field labeled Your Secure Password, and a Generate Password button.](./Assets/03-javascript-homework-demo.png)
+![An app window with the label Password Generator, an input field labeled Your Secure Password, and a Generate Password button.](./assets/images/03-javascript-homework-demo.png)
 
 ## Review
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,46 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+var generatePassword = function(){
+  //declaring variables
+  var includedCharacters = "";
+  var special = /[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/;
+  var numeric = "0123456789";
+  var lowerCase="abcdefghijklmnopqrstuvwxyz";
+  var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  //check to see how long is the desired length of password
+  var desiredLength = window.prompt('How many characters would you like your password to contain?');
+  
+  //convert length into an integer to use later
+  desiredLength = parseInt(desiredLength);
+  console.log("Chose "+ desiredLength + " characters.")
+
+  //check to make sure that desired length meets min length requirements
+  if(desiredLength < 8 || desiredLength > 128){
+    window.alert('Password length must be at least 8 characters and no more than 128');
+    return;
+  }
+
+  var confirmSpecial = window.confirm('Click "OK" to confirm including special characters.');
+  console.log('Special characters is: ' + confirmSpecial);
+  //add if statements here to include special characters
+  
+
+
+  var confirmNumeric = window.confirm('Click "OK" to include numeric characters.');
+  console.log('Numeric characters is: ' + confirmNumeric);
+  //add if statements here to include numeric characters
+
+  var confirmLowerCase = window.confirm('Click "OK" to confirm including lowercase characters.');
+  console.log('LowerCase characters is: ' + confirmLowerCase);
+  //add if statements to include lowercase;
+
+  var confirmUpperCase = window.confirm('Click "OK" to confirm including uppercase characters.');
+  console.log('UpperCase characters is ' + confirmUpperCase);
+  //add if statements to include uppercase;
 
 }
 
